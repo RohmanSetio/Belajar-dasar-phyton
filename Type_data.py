d7a2de48bfc03a25d0353f795a465dcdf8da2b4e
RohmanSetio <156176099+RohmanSetio@users.noreply.github.com>
2024-01-15 23:05:00 +0700

    Add files via upload

---
+++ Type_data.py
@@ -0,0 +1,36 @@
+#type data
+
+# type data interger
+data_interger = 10
+print ("data:",(data_interger))
+print ("data bertype",type(data_interger))
+
+# type data float
+data_float = 5.6
+print ("data:",(data_float))
+print ("data bertype",type(data_float))
+
+# type data string
+data_string = "mansmatcha"
+print ("data:",(data_string))
+print ("data bertype",type(data_string))
+
+# type data boolean
+data_bool = True
+print ("data:",(data_bool))
+print ("data bertype",type(data_bool))
+
+data_bool = False
+print ("data:",(data_bool))
+print ("data bertype",type(data_bool))
+
+#data complex
+data_complex = complex(8,7)
+print ("data:",(data_complex))
+print ("data bertype",type(data_complex))
+
+# type data dari bahasa C
+from ctypes import c_double
+data_c_double = c_double(20.1)
+print ("data:",(data_c_double))
+print ("data bertype",type(data_c_double))
