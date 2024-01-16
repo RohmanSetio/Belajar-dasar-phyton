613308494333c3f6142a069d6debde3aabf715a1
RohmanSetio <156176099+RohmanSetio@users.noreply.github.com>
2024-01-16 13:08:46 +0700

    Add files via upload

---
+++ Casting_data.py
@@ -0,0 +1,16 @@
+data_int = 14
+print ("data =",str(data_int))
+print ("data bertype =",type(data_int))
+
+#data int ke float
+
+data_float = float(data_int)
+print ("data =",str(data_float))
+print ("data bertype =",type(data_float))
+
+#data int ke string 
+
+data_string = str(data_int)
+print ("data =",str(data_string))
+print ("data bertype =",type(data_string))
+
