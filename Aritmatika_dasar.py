12788d92a3a2dc83efa5aba841096345594b6e3c
RohmanSetio <156176099+RohmanSetio@users.noreply.github.com>
2024-01-28 07:19:19 +0700

    Add files via upload

---
+++ Aritmatika_dasar.py
@@ -0,0 +1,31 @@
+a = 10
+b = 5
+
+#penambahan
+hasil = a + b
+print(a,"+",b,"=",hasil)
+
+#pengurangan
+hasil = a - b 
+print(a,"-",b,"=",hasil)
+
+#perkalian
+hasil = a * b
+print(a,"*",b,"=",hasil)
+
+#pembagian
+hasil = a / b
+print(a,"/",b,"=",hasil)
+
+#eksponen
+hasil = a ** b
+print(a,"**",b,"=",hasil)
+
+#floor divison
+hasil = a // b
+print(a,"//",b,"=",hasil)
+
+#modulus
+hasil = a % b
+print(a,"%",b,"=",hasil)
+
